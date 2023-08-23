c992f89398c63aa8c5974df7821b3f9ccfdc67f3
Naoto Nakanishi <nanikasi1024@gmail.com>
2023-08-23 17:35:37 +0900

    norm

--- src/importer.h
+++ src/importer.h
@@ -1,4 +1,14 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   importer.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nanakani <nanakani@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 17:31:15 by nanakani          #+#    #+#             */
+/*   Updated: 2023/08/23 17:31:15 by nanakani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef IMPORTER_H
 # define IMPORTER_H
@@ -12,29 +22,29 @@
 # include "libft/libft.h"
 
 // error handle
-int	is_dup(t_stack *a);
-int	is_valid(int argc, char **argv);
+int		is_dup(t_stack *a);
+int		is_valid(int argc, char **argv);
 void	put_error(void);
 
 // actual control
-int	do_rarb(t_stack **a, t_stack **b, int c, char s);
-int	do_rarrb(t_stack **a, t_stack **b, int c, char s);
-int	do_rrarb(t_stack **a, t_stack **b, int c, char s);
-int	do_rrarrb(t_stack **a, t_stack **b, int c, char s);
+int		do_rarb(t_stack **a, t_stack **b, int c, char s);
+int		do_rarrb(t_stack **a, t_stack **b, int c, char s);
+int		do_rrarb(t_stack **a, t_stack **b, int c, char s);
+int		do_rrarrb(t_stack **a, t_stack **b, int c, char s);
 
 // calc rotation
-int	rarb_b(t_stack *a, t_stack *b, int c);
-int	rarrb_b(t_stack *a, t_stack *b, int c);
-int	rrarb_b(t_stack *a, t_stack *b, int c);
-int	rrarrb_b(t_stack *a, t_stack *b, int c);
+int		rarb_b(t_stack *a, t_stack *b, int c);
+int		rarrb_b(t_stack *a, t_stack *b, int c);
+int		rrarb_b(t_stack *a, t_stack *b, int c);
+int		rrarrb_b(t_stack *a, t_stack *b, int c);
 
-int	rarb_a(t_stack *a, t_stack *b, int c);
-int	rarrb_a(t_stack *a, t_stack *b, int c);
-int	rrarb_a(t_stack *a, t_stack *b, int c);
-int	rrarrb_a(t_stack *a, t_stack *b, int c);
+int		rarb_a(t_stack *a, t_stack *b, int c);
+int		rarrb_a(t_stack *a, t_stack *b, int c);
+int		rrarb_a(t_stack *a, t_stack *b, int c);
+int		rrarrb_a(t_stack *a, t_stack *b, int c);
 
-int	calc_rotate_count_a2b(t_stack *a, t_stack *b);
-int	calc_rotate_count_b2a(t_stack *a, t_stack *b);
+int		calc_rotate_count_a2b(t_stack *a, t_stack *b);
+int		calc_rotate_count_b2a(t_stack *a, t_stack *b);
 
 // operations
 void	pa(t_stack **a, t_stack **b, int j);
@@ -50,22 +60,22 @@ void	sb(t_stack **b, int j);
 void	ss(t_stack **a, t_stack **b, int j);
 
 // sort
-int	is_sorted(t_stack *stack_a);
+int		is_sorted(t_stack *stack_a);
 void	sort_b_by_3(t_stack **stack_a, t_stack **stack_b);
 void	sort_stack(t_stack **stack_a);
 void	sort_three(t_stack **stack_a);
 
 t_stack	*create_stack(int content);
-int	find_where_to_push_b(t_stack *stack_b, int nbr_push);
-int	find_where_to_push_a(t_stack *stack_a, int nbr_push);
+int		find_where_to_push_b(t_stack *stack_b, int nbr_push);
+int		find_where_to_push_a(t_stack *stack_a, int nbr_push);
 void	free_stack(t_stack **lst);
-int	get_index(t_stack *a, int nbr);
+int		get_index(t_stack *a, int nbr);
 t_stack	*get_last(t_stack *lst);
-int	get_stack_size(t_stack *lst);
+int		get_stack_size(t_stack *lst);
 void	stack_add(t_stack **stack, t_stack *stack_new);
 t_stack	*stack_from(int argc, char **argv);
-int	stack_max(t_stack *a);
-int	stack_min(t_stack *a);
+int		stack_max(t_stack *a);
+int		stack_min(t_stack *a);
 
 // utils
 void	free_str(char **lst);
--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nanakani <nanakani@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 17:29:24 by nanakani          #+#    #+#             */
+/*   Updated: 2023/08/23 17:29:24 by nanakani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "importer.h"
 
-int main(int argc, char **argv) 
+int	main(int argc, char **argv)
 {
-	t_stack *stack_a;
+	t_stack	*stack_a;
+
 	if (!is_valid(argc, argv))
 		put_error();
 	stack_a = stack_from(argc, argv);
